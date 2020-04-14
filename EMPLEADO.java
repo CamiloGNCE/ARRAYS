7748a1814a82d27b63d25972e980cef5d2867b2f
Camilo Mejia <CamiloGNCE@gmail.com>
2020-04-14 13:33:46 -0500

    Intercambio inicial de proyecto

---
+++ EMPLEADO.java
@@ -0,0 +1,11 @@
+
+public class EMPLEADO extends PERSONA
+{
+  
+    public EMPLEADO()
+    {
+     
+    }
+
+   
+}
---
+++ PERSONA.java
@@ -0,0 +1,23 @@
+
+public class PERSONA
+{
+    String NOMBRE;
+    String APELLIDO;
+    String GENERO;
+    String TELEFONO;
+    
+    //CONSTRUCTOR 
+    public PERSONA()
+    {
+   
+    }
+    
+    public PERSONA(String NOM,String APE,String GEN,String TELF){
+     this.NOMBRE = NOM;
+     this.APELLIDO = APE;
+     this.GENERO = GEN;
+     this.TELEFONO = TELF;
+    }
+    
+     
+}
